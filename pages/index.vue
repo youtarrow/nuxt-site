d55d756a027f85ccb31a9e32f0662e28fca56045
越中谷優 <y-ecchuya@gajumale.co.jp>
2020-09-01 17:42:17 +0900

    テスト

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">nuxt-site</h1>
+      <h1 class="title">nuxt-sites</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
